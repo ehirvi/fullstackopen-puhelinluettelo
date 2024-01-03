1d6a7e92cd53fcdd838e3361f8100f8d66f34481
ehirvi <eino.hirvi@gmail.com>
2024-01-03 17:48:36 +0200

    3.3 puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id
+    const person = persons.find(person => person.id === Number(id))
+    person ? res.json(person) : res.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
