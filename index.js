a9fe69a163f870811c33117ae94868618d1ffd6b
ehirvi <eino.hirvi@gmail.com>
2024-01-03 17:35:54 +0200

    3.2 puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get("/", (req, res) => {
     res.send("<h1>Home</h1>")
 })
 
+app.get("/info", (req, res) => {
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}</p>`)
+})
+
 app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
