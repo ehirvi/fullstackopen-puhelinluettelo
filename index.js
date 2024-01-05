570a977c29621551ceaf99fdafc9d630a88c6172
ehirvi <eino.hirvi@gmail.com>
2024-01-05 16:02:50 +0200

    3.5 puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -48,6 +50,19 @@ app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
     res.status(204).end()
+})
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body
+    const id = Math.floor(Math.random() * 500)
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    res.json(person)
+    console.log(person)
 })
 
 const PORT = 3001
