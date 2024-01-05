62028b99e625b27931ab14f6e1c7008c747a3d12
ehirvi <eino.hirvi@gmail.com>
2024-01-05 17:44:26 +0200

    3.8 puhelinluettelon backend step8

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
-app.use(morgan("tiny"))
+app.use(morgan((tokens, req, res) => (
+    [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"), "-",
+        tokens["response-time"](req, res), "ms",
+        JSON.stringify(req.body)
+    ].join(" ")
+)))
 
 let persons = [
     {
@@ -82,7 +91,6 @@ app.post("/api/persons", (req, res) => {
         number: body.number
     }
     persons = persons.concat(person)
-    console.log(person)
     res.json(person)
 })
 
