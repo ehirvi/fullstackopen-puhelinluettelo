2ac522acc2ad50c3980a28d9550a6c14ba2cf717
ehirvi <eino.hirvi@gmail.com>
2024-01-05 17:22:39 +0200

    3.7 puhelinluettelon backend step7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
+app.use(morgan("tiny"))
 
 let persons = [
     {
@@ -83,6 +85,12 @@ app.post("/api/persons", (req, res) => {
     console.log(person)
     res.json(person)
 })
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
