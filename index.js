a9c63d8bf548e4af104491ab555020c156bd08c9
ehirvi <eino.hirvi@gmail.com>
2024-01-13 23:54:50 +0200

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan((tokens, req, res) => (
     [
@@ -100,7 +102,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
